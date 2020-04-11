0934a40a9b1d79ed0304898cfa80ee5c258c2063
William <william@cogency.co.za>
2020-04-11 23:33:38 +0200

    simple logic to move around the board

--- app.js
+++ app.js
@@ -11,14 +11,11 @@ const boardFileDir = args[0]
 const movesFileDir = args[1]
 
 // globals
-let boardTitle = null
-let initialBoardSetUp = []
-let initialMoves = []
+let initialStartingPos = null;
+let maxCols = null
+let maxRows = null
 
 // console.log(args)
-
-fs.readFile(boardFileDir, 'utf8', parseBoardFile)
-fs.readFile(movesFileDir, 'utf8', parseMovesFile)
 
 // process imported board file
 async function parseBoardFile(err, contents) {
@@ -35,6 +32,8 @@ async function parseBoardFile(err, contents) {
 
     const numBoardRows = Number(boardContents[1].split(' ')[0])
     const numBoardCols = Number(boardContents[1].split(' ')[1])
+    maxCols = numBoardCols
+    maxRows = numBoardRows
 
     const actualBoardInfo = []
 
@@ -42,7 +41,6 @@ async function parseBoardFile(err, contents) {
         actualBoardInfo.push(boardContents[i])
     }
 
-    // console.log(actualBoardInfo)
 
     // some validation for luls
     // validate the rows/cols
@@ -84,8 +82,11 @@ async function parseBoardFile(err, contents) {
     }
 
     initialBoardSetUp = populatedBoard
+    initialStartingPos = getStartingPosition(initialBoardSetUp)
 
-    // console.log(initialBoardSetUp)
+    fs.readFile(movesFileDir, 'utf8', parseMovesFile)
+
+    console.log(initialBoardSetUp)
 }
 
 async function parseMovesFile(err, contents) {
@@ -108,7 +109,134 @@ async function parseMovesFile(err, contents) {
         }
     }
 
-    initialMoves = contents
-    console.log('the initial moves', initialMoves)
+    initialMoves = lineAsArray
+    startGame()
 }
 
+const checkNextCell = function(row,col) {
+    // console.log(row,col)
+    const nextCell = initialBoardSetUp[row][col]
+    // console.log('this is what is in the next cell', nextCell)
+}
+
+const moveToLeft = function(curPos) {
+    let newRow = (curPos[0])
+    let newCol = (curPos[1]-1)
+    if (newCol < 0) {
+        // console.log('move has no effect')
+        return([curPos[0],curPos[1]])
+    } else {
+        checkNextCell(newRow,newCol)
+        return([newRow, newCol])
+    }
+    
+}
+
+const moveToRight = function(curPos) {
+    let newRow = (curPos[0])
+    let newCol = (curPos[1]+1)
+    if (newCol >= maxCols) {
+        // console.log('move has no effect')
+        return([curPos[0],curPos[1]])
+    } else {
+        checkNextCell(newRow,newCol)
+        return([newRow, newCol])
+    }
+    
+    // console.log('shockingly that worked right', curPos)
+}
+
+const moveDown = function(curPos) {
+    let newRow = (curPos[0]+1)
+    let newCol = (curPos[1])
+    if (newRow >= (maxRows)) {
+        // needToWrap bottom to top
+        // if we wrapping from bottom to top, we just going to the first row right? 
+        // so then newRow would = 0
+        newRow = 0
+        return([newRow,newCol])
+    } else {
+        checkNextCell(newRow,newCol)
+        return([newRow, newCol])
+    }
+    
+    // console.log('shockingly that worked down', curPos)
+}
+
+const moveUp = function(curPos) {
+    let newRow = (curPos[0]-1)
+    let newCol = (curPos[1])
+    if (newRow < 0) {
+        // console.log('should wrap')
+        // need to wrap
+        newRow = maxRows-1
+        return([newRow,newCol])
+    } else {
+        // console.log('moveUp' + newRow + newCol)
+        checkNextCell(newRow,newCol)
+        return([newRow, newCol])
+    }
+    
+    // console.log('shockingly that worked up', newRow, newCol)
+}
+
+const quitGame = function(curPos) {
+    console.log('shockingly that worked', curPos)
+}
+
+
+async function startGame() {
+    console.log('starting game')
+    let currentPos = initialStartingPos
+    for (let i=0; i<initialMoves.length; i++) {
+        try {
+            // console.log('the move before operation', initialMoves[i])
+            let newPos = await movePiece(currentPos, initialMoves[i])
+            console.log('the new pos', newPos)
+           
+            currentPos = newPos
+        } catch (e) {
+            console.error(e)
+        }
+        
+    }
+    console.log('final position', currentPos)
+}
+
+const movePiece = function(currentPos, moveType) {
+    const movesMap = {
+        h: 'moveToLeft',
+        l: 'moveToRight',
+        j: 'moveDown',
+        k: 'moveUp',
+        x: 'quitGame'
+    }
+    // console.log('cur pos', currentPos)
+    return new Promise(function(resolve, reject) {
+        const move = movesMap[moveType]
+        let newPos = eval(move+`([${currentPos[0]}, ${currentPos[1]}])`)
+        // let newPos = currentPos
+        resolve(newPos)
+    })
+    
+}
+
+const getStartingPosition = function(initialBoardSetUp) {
+    const startingChar = 'S'
+    for (let i =0; i < initialBoardSetUp.length; i++) {
+        for (let x =0; x < initialBoardSetUp[i].length; x++) {
+            if (initialBoardSetUp[i][x].toUpperCase() == startingChar) { //technically dont need to upper case it since using lose equality.
+                console.log('start pos', [i,x])
+                return [i,x]
+            }
+        }
+    }
+}
+
+const setCurrentPosition = function(pos) {
+
+}
+
+fs.readFile(boardFileDir, 'utf8', parseBoardFile)
+
+// fs.readFile(movesFileDir, 'utf8', parseMovesFile)
--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 ## run using nodejs. 
 ## takes 2 cmd line arguments. 
 ### First is board definition, second is moves defintion. 
+
+-----------------------------------
+
+### need to validate that the board contains a starting positions (s or S) and a target position (t or T). TODO
